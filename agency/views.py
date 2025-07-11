285fcd1863e9a552c2f100e1f59ff0774bf13e73
Iryna Mishchenko <djrobentab@gmail.com>
2025-07-11 17:31:51 +0200

    Create class-based view for home page

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Topic, Newspaper, Redactor
 
-# Create your views here.
+
+def index(request):
+    """View fuction for the home page of the site."""
+    context = {
+        "num_topics": Topic.objects.count(),
+        "num_newspapers": Newspaper.objects.count(),
+        "num_redactors": Redactor.objects.count(),
+    }
+    return render(
+        request,
+        "agency/index.html",
+        context=context
+    )
+
