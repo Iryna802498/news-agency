7b589d139d371e8218d19c6d578544467fc197f4
Iryna Mishchenko <djrobentab@gmail.com>
2025-07-12 19:36:06 +0200

    Add path DetailView for Topic, Newspaper, Redactor to urls

--- agency/urls.py
+++ agency/urls.py
@@ -1,16 +1,22 @@
 from django.urls import path
 from .views import (index,
                     TopicListView,
+                    TopicDetailView,
                     NewspaperListView,
+                    NewspaperDetailView,
                     RedactorListView,
+                    RedactorDetailView,
                     )
 
 
 urlpatterns = [
     path("", index, name="index"),
     path("topics/", TopicListView.as_view(), name="topic-list"),
+    path("topics/<int:pk>/", TopicDetailView.as_view(), name="topic-detail"),
     path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
+    path("newspapers/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"),
     path("redactors/", RedactorListView.as_view(), name="redactor-list"),
+    path("redactors/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"),
 ]
 
 
