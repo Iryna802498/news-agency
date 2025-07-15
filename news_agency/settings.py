075fc949b8ecaacdda243f9132c0bd3b62988ddf
Iryna Mishchenko <djrobentab@gmail.com>
2025-07-15 22:01:39 +0200

    Refactor and extend settings.py

--- news_agency/settings.py
+++ news_agency/settings.py
@@ -132,6 +132,10 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "agency.Redactor"
 
+LOGIN_REDIRECT_URL = "/"
+
+LOGOUT_REDIRECT_URL = "/"
+
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
