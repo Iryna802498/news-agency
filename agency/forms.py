e387e6378d3d6c29ac89d77c5b4f32916c640dd7
Iryna Mishchenko <djrobentab@gmail.com>
2025-07-15 22:59:41 +0200

    Add search forms for Topic, Newspaper and Redactor models

--- agency/forms.py
+++ agency/forms.py
@@ -46,3 +46,42 @@ class RedactorExperienceUpdateForm(forms.ModelForm):
         if years < 0:
             raise ValidationError("Years of experience cannot be negative.")
         return years
+
+
+class RedactorSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by username"
+            }
+        )
+    )
+
+
+class TopicSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name"
+            }
+        )
+    )
+
+
+class NewspaperSearchForm(forms.Form):
+    title = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by title"
+            }
+        )
+    )
