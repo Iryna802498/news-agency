d0ef5b81e795e423819dfcdaddc26f81dd56b1bc
Iryna Mishchenko <djrobentab@gmail.com>
2025-07-16 23:44:42 +0200

    Add ASSERTS_ROOT to settings

--- news_agency/settings.py
+++ news_agency/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "agency.context_processors.cfg_assets_root",
             ],
         },
     },
@@ -124,6 +125,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+ASSETS_ROOT = "/static/assets"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
