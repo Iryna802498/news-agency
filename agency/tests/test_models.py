c1001408ea745e9f4a14a56df88691e8577b5719
Iryna Mishchenko <djrobentab@gmail.com>
2025-07-16 14:44:07 +0200

    Add tests fot Topic, Newspaper and Redactor models

--- agency/tests/test_models.py
+++ agency/tests/test_models.py
@@ -1,3 +1,62 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
+from agency.models import Topic, Newspaper, Redactor
 
-# Create your tests here.
+
+class ModelTest(TestCase):
+    def test_topic_str(self):
+        topic = Topic.objects.create(name="Test_topic")
+        self.assertEqual(str(topic), topic.name)
+    
+    def test_redactor_str(self):
+        redactor = get_user_model().objects.create(
+            username="test.username",
+            password="test1234",
+            first_name="test_first",
+            last_name="test_last",
+        )
+        self.assertEqual(
+            str(redactor),
+            f"{redactor.username} ({redactor.first_name} {redactor.last_name})"
+        )
+    
+    def test_redactor_get_ablosute_url(self):
+        redactor = get_user_model().objects.create(
+            username="test.user",
+            password="test1234",
+        )
+        expected_url = reverse(
+            "agency:redactor-detail",
+            kwargs={"pk":redactor.pk}
+        )
+        self.assertEqual(redactor.get_absolute_url(), expected_url)
+    
+    def test_newspaper_str(self):
+        topic = Topic.objects.create(name="Test_topic")
+        publishers = get_user_model().objects.create(
+            username="test.user",
+            password="test1234",
+            first_name="test_first",
+            last_name="test_last",
+        )
+        newspaper = Newspaper.objects.create(
+            title="Test Title",
+            content="Test content for newspaper.",
+        )
+        newspaper.topic.add(topic)
+        newspaper.publishers.add(publishers)
+        self.assertEqual(str(newspaper), newspaper.title)
+    
+    def test_create_redactor_with_experience(self):
+        username = "test.user"
+        password = "test1234"
+        experience = 3
+        redactor = get_user_model().objects.create_user(
+            username=username,
+            password=password,
+            years_of_experience=experience,
+        )
+        self.assertEqual(redactor.username, username)
+        self.assertTrue(redactor.check_password(password))
+        self.assertEqual(redactor.years_of_experience, experience)
